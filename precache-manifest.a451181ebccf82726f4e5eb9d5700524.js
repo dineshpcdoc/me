9f712485753580c1c94cfd77c0ef5810d52c02cf
Dinesh Sivakumar <sdk2493@gmail.com>
2021-03-10 18:57:30 +0000

    Updates

---
+++ precache-manifest.a451181ebccf82726f4e5eb9d5700524.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91bf02217206b76085c4ed53f7d1ba85",
+    "url": "/me/index.html"
+  },
+  {
+    "revision": "cc45395cd888f1fef2c5",
+    "url": "/me/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "628631dc51eed1f78789",
+    "url": "/me/static/js/2.699838e2.chunk.js"
+  },
+  {
+    "revision": "cc45395cd888f1fef2c5",
+    "url": "/me/static/js/main.ed8e9592.chunk.js"
+  },
+  {
+    "revision": "66be97a19e235fa95a6c",
+    "url": "/me/static/js/runtime~main.7f73a39c.js"
+  }
+]);
